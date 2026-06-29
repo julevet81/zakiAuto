4aad27720d0da42248ffc2bdf78879f0aa6b51b0
julevet81 <seddik_81@hotmail.fr>
2026-06-29 07:00:35 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-seeders', function () {
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return 'Seeders done';
+});
+
+Route::get('/clear-cache', function () {
+    Artisan::call('permission:cache-reset');
+    Artisan::call('optimize:clear');
+    return 'Cache cleared';
 });
